ff7636bbb6fba86c192321fe33553eca4aa098d0
alessandroamormino <amormino.alessandro97@gmail.com>
2023-02-22 14:24:43 +0100

    feat: finish program

--- script/script.js
+++ script/script.js
@@ -10,17 +10,41 @@
 
 /*
 
-- Chiedere all'utente il numero di chilometri che deve percorrere
-- Chiedere all'utente la sua età
-- calcolare il prezzo del biglietto moltiplicando i kilometri a 0.21€
-- calcolo il prezzo del biglietto scontato al 20% arrotondato alla seconda cifra decimale
-- calcolo il prezzo del biglietto scontato al 40% arrotondato alla seconda cifra decimale
+x Chiedere all'utente il numero di chilometri che deve percorrere
+x Chiedere all'utente la sua età
+x calcolare il prezzo del biglietto moltiplicando i kilometri a 0.21€
+x calcolo il prezzo del biglietto scontato al 20% arrotondato alla seconda cifra decimale (uso la funzione toFixed(n) che arrotonda la cifra 
+  alla n cifra decimale e converte in stringa il numero)
+x calcolo il prezzo del biglietto scontato al 40% arrotondato alla seconda cifra decimale (uso la funzione toFixed(n) che arrotonda la cifra 
+  alla n cifra decimale e converte in stringa il numero)
 ?  SE età dell'utente è minore di 18 (se minorenne)
-    V1 - Stampo il prezzo del biglietto con sconto al 20% 
+    x V1 - Stampo il prezzo del biglietto con sconto al 20% 
 :? ALTRIMENTI SE età dell'utente è maggiore o uguale a 65 (compreso)
-    V2 - Stampo il prezzo del biglietto con sconto al 40% 
+    x V2 - Stampo il prezzo del biglietto con sconto al 40% 
    ALTRIMENTI
-    F1 - Stampo il prezzo del biglietto senza sconti, arrotondato alla seconda cifra decimale
+    x F1 - Stampo il prezzo del biglietto senza sconti, arrotondato alla seconda cifra decimale
 
 */
 
+const numKm = prompt('Quanti chilometri devi percorrere?');
+const userAge = prompt('Quanti anni hai?');
+
+// Calcolo prezzo senza sconti
+let tktPrice = numKm * 0.21;
+tktPrice = tktPrice.toFixed(2);
+
+// Calcolo prezzo con 20% di sconto
+let tktPrice20 = tktPrice - (tktPrice / 100 * 20);
+tktPrice20 = tktPrice20.toFixed(2);
+
+// Calcolo prezzo con 40% di sconto
+let tktPrice40 = tktPrice - (tktPrice / 100 * 40);
+tktPrice40 = tktPrice40.toFixed(2);
+
+if (userAge < 18) {
+    document.writeln(`Il prezzo del biglietto è di ${tktPrice}€ ma siccome sei un minore, il prezzo scontato è di ${tktPrice20}€`);
+} else if (userAge >= 65) {
+    document.writeln(`Il prezzo del biglietto è di ${tktPrice}€ ma siccome sei un over 65, il prezzo scontato è di ${tktPrice40}€`);
+} else {
+    document.writeln(`Il prezzo del biglietto è di ${tktPrice}€`);
+}
